15f05922c4adbe1d508ae9f973d29257e2c11fd7
ioanaverebi <ioanaverebi@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-10-16 21:35:00 +0000

    done with the "CloseTask"

--- trunk/DS/src/usr/speedy/ds/CloseTask.java
+++ trunk/DS/src/usr/speedy/ds/CloseTask.java
@@ -3,6 +3,12 @@ package usr.speedy.ds;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -27,12 +33,13 @@ public class CloseTask extends Tasks {
 	protected void printContent(PrintWriter out, HttpServletRequest request) {
 		try {
 			HttpSession session = request.getSession(true);
-			String result = (String)session.getAttribute("addTaskMessage");
+			String result = (String)session.getAttribute("closeTaskMessage");
 			if (result == null){
 				util.printFromFile(out, "tasks/closeTask.html");
 				session.setAttribute("closeTaskMessage", null);
 			}
 			else {
+				System.out.println(">>>>"+result);
 				util.printReplacedText(out, "tasks/printMessage.html", "templateMessage", result);
 				session.setAttribute("closeTaskMessage", null);
 			}
@@ -43,16 +50,62 @@ public class CloseTask extends Tasks {
 		}
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+
 		String taskName = request.getParameter("taskname");
 		if (taskName == null || taskName.trim().equals("")){
 			message("Task name cannot be empty", request.getSession());
 		}
-		message("Task was closed successfully", request.getSession());
+		if (closeTask(taskName, request))
+			message("Task was closed successfully", request.getSession());
+		doGet(request, response);
 	}
-	
+
+	private boolean closeTask(String taskName, HttpServletRequest request){
+		HttpSession session = request.getSession(true);
+		Connection connection = (Connection) session.getAttribute("connection");
+		if (connection != null){
+			Statement stmt;
+			try {
+				stmt = connection.createStatement();
+				ResultSet duplicateName = stmt.executeQuery("SELECT COUNT(*) AS exista FROM tasks WHERE name='"+taskName+"' and status='open'");
+				duplicateName.next();
+				int exists = duplicateName.getInt("exista");
+				if (exists != 1){
+					if (exists == 0)
+						message("Task name does not exist or is closed", request.getSession());
+					else
+						message("The database is malformed: duplicate task names", request.getSession());
+					return false;
+				}
+				else{
+					stmt.execute("UPDATE tasks SET status='closed' WHERE name='"+taskName+"'");
+					List<String> allAssignments = new ArrayList<String>();
+					List<String> allProg = new ArrayList<String>();
+					ResultSet records = stmt.executeQuery("SELECT * FROM assingments WHERE tskID = (SELECT id FROM tasks WHERE name='"+taskName+"')");
+					while(records.next()){
+						allAssignments.add(records.getString("assID"));
+						allProg.add(records.getString("prgID"));
+					}
+					for (String prog : allProg) {
+						stmt.execute("UPDATE programmers SET status='available' WHERE id="+prog);
+					}
+					for (String assign : allAssignments) {
+						stmt.execute("DELETE FROM assingments WHERE assID = "+assign);
+					}
+				}
+				return true;
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				message("Error inserting data", session);
+			}
+		}
+		return false;
+	}
+
 	private void message(String string, HttpSession session) {
 		session.setAttribute("closeTaskMessage", string);
 	}
