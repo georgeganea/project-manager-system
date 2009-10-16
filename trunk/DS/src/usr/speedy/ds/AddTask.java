4ed6eb640fd87577d6f28c47b18271d135ce557d
ioanaverebi <ioanaverebi@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-10-16 20:28:54 +0000

    done with the "AddTask"

--- trunk/DS/src/usr/speedy/ds/AddTask.java
+++ trunk/DS/src/usr/speedy/ds/AddTask.java
@@ -4,8 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -61,8 +64,9 @@ public class AddTask extends Tasks {
 			int noOfProg = Integer.parseInt(noOfProgrammers);
 			if (noOfProg <= 0)
 				message("Invalid number of programmers", request.getSession());
-			addToDataBase(taskName, noOfProg, request);
-			message("Task "+taskName+" succesfully added", request.getSession());
+			boolean result = addToDataBase(taskName, noOfProg, request);
+			if (result)
+				message("Task "+taskName+" succesfully added", request.getSession());
 		}
 		catch(NumberFormatException e){
 			message("Invalid number format", request.getSession());
@@ -71,21 +75,49 @@ public class AddTask extends Tasks {
 	}
 
 
-	private void addToDataBase(String taskName, int noOfProg, HttpServletRequest request) {
+	private boolean addToDataBase(String taskName, int noOfProg, HttpServletRequest request) {
+		List<String> allNames = new ArrayList<String>();
 		HttpSession session = request.getSession(true);
 		Connection connection = (Connection) session.getAttribute("connection");
 		if (connection != null){
 			Statement stmt;
 			try {
 				stmt = connection.createStatement();
-				stmt.execute("INSERT INTO programmers(name) VALUES ('"+taskName+"')");
-				stmt.equals("COMMIT");
+				ResultSet recordCnt = stmt.executeQuery("SELECT COUNT(*) AS cnt FROM programmers WHERE status='available'");
+				recordCnt.next();
+				int count = recordCnt.getInt("cnt");
+				if (count < noOfProg){
+					message("The requested number of programmers is not available", session);
+					return false;
+				}
+				else{
+					ResultSet records = stmt.executeQuery("SELECT * FROM programmers WHERE status='available'");
+					int cnt = 0;
+					while(records.next()){
+						String str = records.getString("id");
+						allNames.add(str);
+						cnt++;
+						if (cnt == noOfProg)
+							break;
+					}//end while loop
+					for (String name : allNames) {
+						stmt.execute("UPDATE programmers SET status='busy' WHERE id="+name);
+					}
+					stmt.execute("INSERT INTO tasks(name, status, nopeople) VALUES ('"+taskName+"'"+",'open',"+noOfProg+")");
+					ResultSet lastInsertedTask = stmt.executeQuery("SELECT * FROM tasks WHERE id = (SELECT MAX(id) FROM tasks)");
+					lastInsertedTask.next();
+					int lastInserted = lastInsertedTask.getInt("id");
+					for (String string : allNames) {
+						stmt.execute("INSERT INTO assingments(prgID, tskID) VALUES ("+string+","+lastInserted+")");
+					}
+					return true;
+				}
 			} catch (SQLException e) {
 				e.printStackTrace();
-				message("Error inserting data", request.getSession());
+				message("Error inserting data", session);
 			}
-			//connection.createStatement("INSERT INTO programmers(name) VALUES ('"+taskName+"');")
 		}
+		return false;
 	}
 
 	private void message(String string, HttpSession session) {
