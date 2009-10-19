261da780c5c141bab4de3de28a68a70607145cdd
ioanaverebi <ioanaverebi@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-10-19 00:56:43 +0000

    Carmen:: Overview

--- trunk/DS/src/usr/speedy/ds/Overview.java
+++ trunk/DS/src/usr/speedy/ds/Overview.java
@@ -1,5 +1,13 @@
 package usr.speedy.ds;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class Overview
@@ -15,8 +23,31 @@ public class Overview extends TemplateServlet {
         // TODO Auto-generated constructor stub
     }
 
+	@Override
+	protected void templateMethod(PrintWriter out, HttpServletRequest request) {
+		try {
+			util.printFromFile(out, "overview/top.html", "Overview");
+			printContent(out, request);
+			util.printFromFile(out, "overview/bottom.html");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	protected void printContent(PrintWriter out, HttpServletRequest request) {
+		try {
+			util.printFromFile(out, "overview/content.html");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
-    protected String getActivePage() {
+	protected String getActivePage() {
 		return "Overview";
 	}
 }
---
+++ trunk/DS/src/usr/speedy/ds/Workload.java
@@ -0,0 +1,91 @@
+package usr.speedy.ds;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Workload
+ */
+public class Workload extends Overview {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see TemplateServlet#TemplateServlet()
+     */
+    public Workload() {
+        super();
+    }
+
+    protected void printContent(PrintWriter out, HttpServletRequest request) {
+    	try {
+    		int cnt1,cnt2;
+    		String message;
+    		cnt1 = getNoOfProgrammers(request);
+    		cnt2 = getNoOfAvailableProgrammers(request);
+    		if (cnt1!=0)
+    			message = new String("<p>The current workload is "+ (((float)(cnt1-cnt2)/cnt1))*100+"% </p>");
+    		else 
+    			message = new String("Workload information not available.");
+    		util.printReplacedText(out, "overview/printMessage.html", "templateMessage", message);
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+		
+	}
+  
+	private int getNoOfProgrammers(HttpServletRequest request) {
+		int count = 0;
+		HttpSession session = request.getSession(true);
+		Connection connection = (Connection) session.getAttribute("connection");
+		if (connection != null){
+			Statement stmt;
+			try {
+				stmt = connection.createStatement();
+				ResultSet recordCnt = stmt.executeQuery("SELECT COUNT(*) AS cnt FROM programmers");
+				recordCnt.next();
+				count = recordCnt.getInt("cnt");
+				
+			}
+			 catch (SQLException e) {
+				e.printStackTrace();
+				message("Error getting data from the database", session);
+			}
+		}
+		return count;
+	}
+	
+	private int getNoOfAvailableProgrammers(HttpServletRequest request) {
+		int count = 0;
+		HttpSession session = request.getSession(true);
+		Connection connection = (Connection) session.getAttribute("connection");
+		if (connection != null){
+			Statement stmt;
+			try {
+				stmt = connection.createStatement();
+				ResultSet recordCnt = stmt.executeQuery("SELECT COUNT(*) AS cnt FROM programmers WHERE status='available'");
+				recordCnt.next();
+				count = recordCnt.getInt("cnt");
+				
+			}
+			 catch (SQLException e) {
+				e.printStackTrace();
+				message("Error getting data from the database", session);
+			}
+		}
+		return count;
+	}
+
+	private void message(String string, HttpSession session) {
+		session.setAttribute("workloadMessage", string);
+	}
+}
