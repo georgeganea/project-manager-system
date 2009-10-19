b45334861f54d73ae5f79aa80f2e621e4e953f2c
ioanaverebi <ioanaverebi@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-10-19 00:56:43 +0000

    Carmen:: Overview

---
+++ DS/src/usr/speedy/ds/AvailableProgrammers.java
@@ -0,0 +1,82 @@
+package usr.speedy.ds;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AvailProg
+ */
+public class AvailableProgrammers extends Overview {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see Overview#Overview()
+     */
+    public AvailableProgrammers() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void printContent(PrintWriter out, HttpServletRequest request) {
+    	try {
+    		String message="\n";
+    		int i = 0;
+    		ArrayList <String> programmers = getListOfAvailProgrammers(request);
+    	while (i < programmers.size()){
+    		message = message + programmers.get(i)+"<BR>";
+    		i++;
+    	}
+    	if (programmers.size()>0)
+    		util.printReplacedText(out, "overview/availprog.html", "templateMessage", message);
+    	else
+    		util.printReplacedText(out, "overview/printMessage.html", "templateMessage", "Right now, there are no available programmers.");
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+		
+	}
+	
+   
+    
+    private ArrayList<String> getListOfAvailProgrammers(HttpServletRequest request) {
+    	ArrayList<String> result = new ArrayList<String>();
+		HttpSession session = request.getSession(true);
+		Connection connection = (Connection) session.getAttribute("connection");
+		if (connection != null){
+			Statement stmt;
+			try {
+				stmt = connection.createStatement();
+				ResultSet recordCnt = stmt.executeQuery("SELECT * FROM programmers WHERE status='available';");
+				while (recordCnt.next()) {
+				      String name = recordCnt.getString(1);
+				      result.add(name);
+				}
+				
+			}
+			 catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+    
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	//	Object taskName = request.getParameter("taskname");
+		
+	}
+
+}
---
+++ DS/src/usr/speedy/ds/CurrentTasks.java
@@ -0,0 +1,82 @@
+package usr.speedy.ds;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CurrentTasks
+ */
+public class CurrentTasks extends Overview {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see Overview#Overview()
+     */
+    public CurrentTasks() {
+        super();
+    }
+
+    
+    protected void printContent(PrintWriter out, HttpServletRequest request) {
+    	try {
+    		String message="\n";
+    		int i = 0;
+    		ArrayList <String> tasks = getListOfTasks(request);
+    	while (i < tasks.size()){
+    		message = message + tasks.get(i)+"<BR>";
+    		i++;
+    	}
+    	if (tasks.size()>0)
+    		util.printReplacedText(out, "overview/currenttasks.html", "templateMessage", message);
+    	else
+    		util.printReplacedText(out, "overview/printMessage.html", "templateMessage", "Right now, there are no tasks.");
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+		
+	}
+    
+    private ArrayList<String> getListOfTasks(HttpServletRequest request) {
+    	ArrayList<String> result = new ArrayList<String>();
+		HttpSession session = request.getSession(true);
+		Connection connection = (Connection) session.getAttribute("connection");
+		if (connection != null){
+			Statement stmt;
+			try {
+				stmt = connection.createStatement();
+				ResultSet recordCnt = stmt.executeQuery("SELECT * FROM tasks;");
+				while (recordCnt.next()) {
+				      String name = recordCnt.getString(2);
+				      String status = recordCnt.getString(3);
+				      String programmers = recordCnt.getString(4);
+				      result.add(name+"	"+status+"	"+programmers);
+				}
+				
+			}
+			 catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	//	Object taskName = request.getParameter("taskname");
+		
+	}
+
+}
