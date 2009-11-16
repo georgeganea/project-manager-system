0a2ce19f3f907482a4a16d66e2c7ae40a70f87c0
georgeganea <georgeganea@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-11-16 21:31:36 +0000

    moved methods to assignment

--- trunk/Project/ejbModule/org/speedy/ProgrammerDAORemote.java
+++ trunk/Project/ejbModule/org/speedy/ProgrammerDAORemote.java
@@ -1,12 +1,7 @@
 package org.speedy;
-import java.util.List;
-
 import javax.ejb.Remote;
 
 @Remote
 public interface ProgrammerDAORemote {
-
-	public List<Programmer> getAllProgrammers();
-	public Task getTaskForProgrammer(String name);
 	public boolean insert(Programmer programmer);
 }
---
+++ trunk/Project/ejbModule/org/speedy/assignment/AssignmentDAORemote.java
@@ -0,0 +1,7 @@
+package org.speedy.assignment;
+import javax.ejb.Remote;
+
+@Remote
+public interface AssignmentDAORemote {
+
+}
