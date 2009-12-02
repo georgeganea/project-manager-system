edbda8c06f150330a0a83600af1a0899a44c7ecc
ioanaverebi <ioanaverebi@7db8eff2-b4f7-11de-92a3-373a1addbe3b>
2009-12-02 22:56:12 +0000

    overview (status + tree)

---
+++ DS3-client/src/usr/speedy/ds/client/overview/GetBusyProgrammers.java
@@ -0,0 +1,32 @@
+
+package usr.speedy.ds.client.overview;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getBusyProgrammers complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getBusyProgrammers">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getBusyProgrammers")
+public class GetBusyProgrammers {
+
+
+}
